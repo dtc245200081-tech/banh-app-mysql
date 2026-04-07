ad1ef9e05ca3e47695fb061fdbfa26a5974cb451
dtc245200081-tech <dtc245200081@ictu.edu.vn>
2026-04-07 12:52:11 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/database');
 
 const Order = sequelize.define('Order', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
