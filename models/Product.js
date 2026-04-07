19ca5b71ab75e389a54c971a23d4b63c46cdc60a
dtc245200081-tech <dtc245200081@ictu.edu.vn>
2026-04-07 12:49:49 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/database');
 
 const Product = sequelize.define('Product', {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
